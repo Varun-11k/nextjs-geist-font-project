44593af4e064fe8113c0c8ece359fbeff6f7e3e6
Varun Kumar <vkvarun04712@gmail.com>
2025-08-31 16:27:39 +0530

    Add src/app/classroom/page.tsx 1756637857636

---
+++ src/app/classroom/page.tsx
@@ -0,0 +1,9 @@
+import TeacherInterface from "@/components/RemoteClassroom/TeacherInterface";
+
+export default function ClassroomPage() {
+  return (
+    <div>
+      <TeacherInterface />
+    </div>
+  );
+}
